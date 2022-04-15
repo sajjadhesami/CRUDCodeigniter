4fb25ea98c64b2bbc60eddfcd46cb7a1310d798f
sajjadhesami <34142871+sajjadhesami@users.noreply.github.com>
2022-04-15 15:51:44 +0430

    Stage 1

---
+++ application/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+
+class Home extends CI_Controller
+{
+	public function index(){
+		$this->load->view('templates/header');
+		$data["title"]="Welcome to Simple CodeIgniter CMS";
+		$this->load->view('home',$data);
+		$this->load->view('templates/footer');
+	}
+}
---
+++ application/views/templates/header.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+	<title>Simple PHP CodeIgniter CMS</title>
+</head>
+<nav class="navbar">
+<div class="container">
+	<div class="navbar-header">
+		<a class="navbar-brand" href="<?php echo base_url(); ?>"/>
+	</div>
+	<div class="navbar">
+		<ul>
+			<li>
+				<a href="<?php echo base_url(); ?>">Home</a>
+			</li>
+			<li>
+				<a href="<?php echo base_url(); ?>login.php">Login</a>
+			</li>
+		</ul>
+	</div>
+
+</div>
+</nav>
+<body>
+<?php ?>
